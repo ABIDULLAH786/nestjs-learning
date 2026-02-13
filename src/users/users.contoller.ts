a2338c05ec31b1477239c5c61336f8a17e3b5bcf
Abid Ullah <abidullah.bsef18@iba-suk.edu.pk>
2026-02-13 08:12:47 +0500

    params validation added

--- src/users/users.contoller.ts
+++ src/users/users.contoller.ts
@@ -1,25 +1,44 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Patch, Post, Put, Query } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
 
     @Get("/:id{/:optional}")
-    getUsers(@Param() params: any, @Query() query: any) {
+    getUsers(
+        @Param("id", ParseIntPipe) id: any,
+        @Query("limit", new DefaultValuePipe(10), ParseIntPipe) limit: number,
+        @Query("page", new DefaultValuePipe(1), ParseIntPipe) page: number
+    ) {
         return {
             data: {
-                params,
-                query
+                id,
+                query: {
+                    limit,
+                    page
+                }
             },
             message: 'success'
         };
     }
 
     @Post()
-    ceateUsers(@Body() request: any){
+    ceateUsers(@Body() request: any) {
         return {
             data: request,
             message: 'success'
         }
+    }
+
+
+    @Patch("/:id")
+    updateUsers(@Param("id", ParseIntPipe) id: number, @Body() body: any) {
+        return {
+            data: {
+                id,
+                body
+            },
+            message: 'success'
+        };
     }
 
 }
